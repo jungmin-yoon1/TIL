b1b33af91fee84845221a35ef6fecb7b8522d93e
jungmin-yoon1 <58179712+jungmin-yoon1@users.noreply.github.com>
2021-07-07 11:29:11 +0900

    HTML main

---
+++ C++ study/상속과 포함/main_HTML.cpp
@@ -0,0 +1,17 @@
+// HTML & DocWriter h, cpp 이용
+#include "HTMLWriter.h"
+
+int main()
+{
+	//HTML Writer 객체 생성
+	HTMLWriter hw("test.html", "HTMLWriter Content");
+
+	//DocWriter 클래스의 포인터로 객체를 가리킴
+	DocWriter* pDW = &hw; //부모 객체의 포인터로 자식 객체를 가리키는 경우는 가능, 역은 성립 불가
+	//DocWriter& dw=hw; // DocWriter 클래스의 레퍼런스로 객체 참조 
+
+	//파일에 저장
+	pDW->Write(); //DocWriter::Write() 함수가 호출됨, 실제 객체가 무엇이던간에 상관없이 포인터의 타입을 기준으로 호출될 함수 결정됨
+	
+	return 0;
+}
